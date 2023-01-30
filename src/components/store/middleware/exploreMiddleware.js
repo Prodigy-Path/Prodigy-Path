0a066a6ae2e53d4318bef8133d8988be5be07298
Seth <sethparkerpierce@icloud.com>
2023-01-30 18:09:04 -0500

    adds explore page functionality and stlying

---
+++ src/components/store/middleware/exploreMiddleware.js
@@ -0,0 +1,18 @@
+import fetchApi from '../../../utility/fetchApi';
+
+const exploreMiddleware = (store) => (next) => async (action) => {
+  if (action.payload?.action === 'search') {
+    let url = `${process.env.REACT_APP_SERVER}/users`;
+    let body = null;
+    let method = 'get';
+    let response = await fetchApi(url, body, method);
+    if(action.payload.userRole === 'protege'){
+      action.payload = response.filter(user => user.role === 'mentor');
+    } else {
+      action.payload = response.filter(user => user.role === 'protege');
+    }
+  }
+  next(action);
+};
+
+export default exploreMiddleware;
