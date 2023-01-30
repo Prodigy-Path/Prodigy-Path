1f1ed1e4c90f6793c740e5b073c2a888d79e7f92
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-30 09:33:18 -0500

    remove unused variables for deployment

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,4 @@
 /** @format */
-
-import HeaderComponent from '../Header';
 import Login from '../signupLogin/Login';
 import SignUp from '../signupLogin/SignUp';
 import { Route, Routes } from 'react-router-dom';
