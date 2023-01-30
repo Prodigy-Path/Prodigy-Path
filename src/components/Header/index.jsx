635395c4ac9032880cac808d64d2a816357ddb71
tm-LBenson <tm.lewisbenson@gmail.com>
2023-01-30 09:58:07 -0500

    Remove anchor tags in drawer to useNavigate hook

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   Header,
   Group,
@@ -13,11 +13,10 @@ import {
   Button,
   Burger,
   Anchor,
-
 } from '@mantine/core';
 import { useSelector, useDispatch } from 'react-redux';
 import { setOpened, setDrawer } from '../store/drawerSlice';
-// import { AuthContext } from '../../context/Auth';
+
 import { logout } from '../store/loginSlice';
 const HeaderComponent = () => {
   const dispatch = useDispatch();
@@ -27,21 +26,30 @@ const HeaderComponent = () => {
   const handleLogout = () => {
     dispatch(logout());
   };
-
-
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(setDrawer());
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [opened]);
   return (
-    <Header height={75} fixed={true} className='header'>
-      <Group position='apart' className='header__group'>
-        <Text className='header__group__title'>ProdigyPath</Text>
-        <Group position='apart' className='header__group__nav'>
+    <Header
+      height={75}
+      fixed={true}
+      className="header"
+    >
+      <Group
+        position="apart"
+        className="header__group"
+      >
+        <Text className="header__group__title">ProdigyPath</Text>
+        <Group
+          position="apart"
+          className="header__group__nav"
+        >
           <SegmentedControl
-            className='header__group__nav__segment'
+            className="header__group__nav__segment"
             transitionDuration={500}
-            transitionTimingFunction='linear'
+            transitionTimingFunction="linear"
             data={[
               { label: 'Dashboard', value: 'dashboard' },
               { label: 'Tasks', value: 'tasks' },
@@ -51,8 +59,8 @@ const HeaderComponent = () => {
           {isLoggedIn ? (
             <Button
               onClick={handleLogout}
-              className='header__group__nav__signup'
-              size='md'
+              className="header__group__nav__signup"
+              size="md"
               styles={(theme) => ({
                 root: {
                   '&:hover': {
@@ -65,12 +73,12 @@ const HeaderComponent = () => {
                 },
               })}
             >
-              <Link to='/'>Logout</Link>
+              <Link to="/">Logout</Link>
             </Button>
           ) : (
             <Button
-              className='header__group__nav__signup'
-              size='md'
+              className="header__group__nav__signup"
+              size="md"
               styles={(theme) => ({
                 root: {
                   '&:hover': {
@@ -83,13 +91,13 @@ const HeaderComponent = () => {
                 },
               })}
             >
-              <Link to='/login'>Login</Link>
+              <Link to="/login">Login</Link>
             </Button>
           )}
         </Group>
         <Burger
-          className='header__group__burger'
-          color='#E6DDC4'
+          className="header__group__burger"
+          color="#E6DDC4"
           opened={opened}
           onClick={() => dispatch(setOpened())}
           title={title}
@@ -97,16 +105,20 @@ const HeaderComponent = () => {
 
         <SegmentedControl
           className={`header__group__mobileMenu ${menuClass} `}
-          orientation='vertical'
+          orientation="vertical"
           transitionDuration={500}
-          transitionTimingFunction='linear'
+          transitionTimingFunction="linear"
           data={[
             {
               value: 'dashboard',
               label: (
                 <Center>
-                  <Box>
-                    <Anchor href='/'>Dashboard</Anchor>
+                  <Box
+                    onClick={() => {
+                      navigate('/');
+                    }}
+                  >
+                    Dashboard
                   </Box>
                 </Center>
               ),
@@ -115,8 +127,12 @@ const HeaderComponent = () => {
               value: 'tasks',
               label: (
                 <Center>
-                  <Box>
-                    <Link to={'/tasks'}>Tasks</Link>
+                  <Box
+                    onClick={() => {
+                      navigate('/tasks');
+                    }}
+                  >
+                    Tasks
                   </Box>
                 </Center>
               ),
@@ -125,10 +141,12 @@ const HeaderComponent = () => {
               value: 'explore',
               label: (
                 <Center>
-                  <Box>
-                    <Anchor>
-                      <Link to={'/explore'}>Explore</Link>
-                    </Anchor>
+                  <Box
+                    onClick={() => {
+                      navigate('/');
+                    }}
+                  >
+                    Explore
                   </Box>
                 </Center>
               ),
