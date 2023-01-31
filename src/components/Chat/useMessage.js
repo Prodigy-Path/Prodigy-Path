bb41217624983de55ed95073286fc1a3b180f082
Stevenrej <steveo732@gmail.com>
2023-01-31 16:38:06 -0600

    added id to not allow duplicate messages

--- src/components/Chat/useMessage.js
+++ src/components/Chat/useMessage.js
@@ -1,13 +1,11 @@
 
 export const joinRoom = (data ) => {
   const { socket, text } = data;
-
-
   socket.emit('JOIN', text);
 };
 
 export const sendMessage = (data) => {
   console.log(data);
-  const { socket, text } = data;
-  socket.emit('SEND_MESSAGE', text);
+  const { socket, text, id } = data;
+  socket.emit('SEND_MESSAGE', text, id);
 };
