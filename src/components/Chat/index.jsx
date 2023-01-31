c54d265434aff898314a5213f8651eb2e80148ca
Stevenrej <steveo732@gmail.com>
2023-01-30 21:45:00 -0600

    updated Chat component - moved Receive message to this file from useMessage - moved User Connected to this file from useMessage -Thunk functions added

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,42 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
-import messageInterface from './useMessage';
-import { joinRoom, sendMessage } from './useMessage';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { joinRoomThunk, sendMessageThunk } from '../store/chatSlice';
+import io from "socket.io-client";
+// import messageInterface from './useMessage';
+const socket = io.connect("http://localhost:3002");
 
 
 const Chat = () => {
 
-  const endpoint = 'http://localhost:3002'
-
-  const io = socketIOClient(endpoint);
-
-  const [roomName, setRoomName] = useState('');
-  const [roomList, setRoomList] = useState([]);
-  const [messages, setMessages] = useState([]);
 
 
-  useEffect(() => (
-    messageInterface(io)
-  ), []);
+  const dispatch = useDispatch();
+  const { roomList, messages } = useSelector(state => state.chat);
 
 
-  console.log(messages);
+  // console.log('messaages:', messages);
+
+
+  useEffect(() => {
+    socket.on('RECEIVE_MESSAGE', (data) => {
+      console.log('YAY GOT MESSAGE', data);
+    });
+  
+    socket.on('USER_CONNECTED', (data) => {
+      console.log('USER CONNECTED YAY', data);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [socket]);
+  
 
   function handleSubmitMessage(e) {
     e.preventDefault();
-    sendMessage(io, e.target.text.value)
+    const text = e.target.text.value;
+    dispatch(sendMessageThunk({ socket, text }))
   }
   
+  function handleJoinRoom(e) {
+    e.preventDefault();
+    const text = e.target.text.value;
+    dispatch(joinRoomThunk({ socket, text }))
+  }
 
   return (
     <div>
       <div>
       </div>
       <div>
-
-        <input type="text" value={roomName} onChange={(e) => setRoomName(e.target.value)} />
-        <button onClick={() => joinRoom(io, roomName)}>Join Room</button>
-
+      <form onSubmit={handleJoinRoom}>
+          <textarea name='text'/>
+          <button type='submit'>Room</button>
+        </form>
         <ul>
           {roomList.map((room, index) => (
             <li key={index}>{room}</li>
