e1ee5db5ef95c84ac836adca0bddff6f964c9faf
Stevenrej <steveo732@gmail.com>
2023-01-30 21:47:07 -0600

    removed uneeded function

--- src/components/Chat/useMessage.js
+++ src/components/Chat/useMessage.js
@@ -1,29 +1,13 @@
 
+export const joinRoom = (data ) => {
+  const { socket, text } = data;
 
 
-export default function messageInterface(io) {
-
-    io.on('proofOfLife', (data) => {
-      console.log(data);
-    });
-
-    io.on('ROOMS', (data) => {
-console.log(data);
-    });
-
-  io.on('RECEIVE_MESSAGE', (data) => {
-    console.log(data);
-  });
-}
-
-
-export const joinRoom = (io, roomName) => {
-  if (!io) return;
-
-  io.emit('JOIN', roomName);
+  socket.emit('JOIN', text);
 };
 
-export const sendMessage = (io, message) => {
-  io.emit('SEND_MESSAGE', message);
-  console.log(message);
+export const sendMessage = (data) => {
+  console.log(data);
+  const { socket, text } = data;
+  socket.emit('SEND_MESSAGE', text);
 };
