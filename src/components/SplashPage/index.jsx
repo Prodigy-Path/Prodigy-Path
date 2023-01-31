7a86a7e9e976a519854eacdffd88e85033f80632
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-01-30 19:47:03 -0500

    basic layout for splash page

--- src/components/SplashPage/index.jsx
+++ src/components/SplashPage/index.jsx
@@ -1,5 +1,7 @@
 /** @format */
 
+import Card from './Card';
+
 const SplashPage = () => {
   return (
     <>
@@ -7,7 +9,40 @@ const SplashPage = () => {
         <section className="splash__hero">
           <h1>Splash</h1>
         </section>
-        <section className="splash__feature"></section>
+        <section className="splash__feature">
+          <div className="splash__row">
+            <Card>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in
+            </Card>
+            <Card>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in
+            </Card>
+          </div>
+          <div className="splash__row">
+            <Card>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in
+            </Card>
+            <Card>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in
+            </Card>
+          </div>
+        </section>
       </div>
     </>
   );
