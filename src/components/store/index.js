fda34584829e6d50e4151303d95672e8afef01be
Stevenrej <steveo732@gmail.com>
2023-01-30 21:48:48 -0600

    imported chatSlice and chat middleware

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -5,15 +5,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import drawerSlice from './drawerSlice';
 import logger from './middleware/logger';
 import loginSlice from './loginSlice';
+import chatSlice from './chatSlice';
 import loginMiddleware from './middleware/loginMiddleware';
 import signUpMiddleware from './middleware/signUpMiddleware';
+import chatMiddleware from './middleware/chatMiddleware';
 
 const store = configureStore({
   reducer: {
     drawer: drawerSlice,
     login: loginSlice,
+    chat: chatSlice,
   },
-  middleware: [logger, loginMiddleware, signUpMiddleware],
+  middleware: [logger, loginMiddleware, signUpMiddleware, chatMiddleware],
 });
 
 export default store;
