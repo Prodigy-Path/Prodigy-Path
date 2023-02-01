c1ba47058cd6730ebef917d81f834b15bf6f0c15
Stevenrej <steveo732@gmail.com>
2023-01-31 20:31:35 -0600

    functionality imported to render users connections

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -2,29 +2,33 @@
 
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { joinRoomThunk, sendMessageThunk } from '../store/chatSlice';
+import { joinRoomThunk, sendMessageThunk, getChats } from '../store/chatSlice';
+import { Select } from '@mantine/core';
 import io from 'socket.io-client';
 const socket = io.connect(process.env.REACT_APP_SERVER);
 
 const Chat = () => {
   const dispatch = useDispatch();
-  const {  messages, roomName } = useSelector((state) => state.chat);
+  const { messages, roomName, chatConnection } = useSelector((state) => state.chat);
   const { user } = useSelector((state) => state.login);
-
 
   const [isChatVisible, setIsChatVisible] = useState(false);
 
-  const testMentors = ['jim', 'tom', 'joe', 'craig', 'ronny']
+  const filterConnection = chatConnection.filter((item) => user._id === item.mentor || user._id === item.protege);
 
-  console.log(user);
+  const dataRender = filterConnection.map((item) => item._id )
 
   let name = user.name
+
+  useEffect(() => {
+    dispatch(getChats({action: 'getChats'}))
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
 
   useEffect(() => {
     socket.on('RECEIVE_MESSAGE', ({ text, id }) => {
 
-      console.log('YAY GOT MESSAGE', text, id);
       dispatch(sendMessageThunk({ text: text, id: id }));
     });
 
@@ -34,6 +38,11 @@ const Chat = () => {
     socket.on('proofOfLife', (data) => {
       console.log(data);
     });
+    // socket.on('USER_DISCONNECTED', (obj) => {
+
+    //   const { text, room } = obj
+    //   dispatch(sendMessageThunk({ socket, text, id: crypto.randomUUID(), room }));
+    // })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socket]);
 
@@ -55,23 +64,25 @@ const Chat = () => {
     console.log(room);
     let text = room;
     socket.emit("LEAVE_ROOM", roomName);
+    dispatch(sendMessageThunk({ socket, text, id: null }));
     dispatch(joinRoomThunk({ socket, text }));
   }
 
 
 
+
   return (
+    <>
     <div className="chat_open">
       {isChatVisible && (
         <div className="chat">
-          <div className="chat__join">
-            <ul>
-              <button onClick={() => setIsChatVisible(false)}>Close Chat</button>
-              {testMentors.map((room, index) => (
-                <div onClick={() => handleJoinRoom(room)} key={index}>{room}</div>
-                ))}
-            </ul>
-          </div>
+          <Select
+          className='chat__select'
+          data={dataRender}
+          label='Choose Room'
+          onChange={(room) => handleJoinRoom(room)}
+          />
+            <p className="close_button " onClick={() => setIsChatVisible(false)}>X</p>
           <div className="chat__message">
             <form onSubmit={handleSubmitMessage}>
               <textarea name="text" />
@@ -79,18 +90,19 @@ const Chat = () => {
             </form>
           </div>
           <div className="chat__window">
-            <ul>
-              {messages.map((msg, index) => (
-                <li key={index}>{`${msg.text}`}</li>
+              <ul>
+                {messages.map((msg, index) => (
+                  <li key={index}>{`${msg.text}`}</li>
                 ))}
-            </ul>
+              </ul>
           </div>
         </div>
       )}
       {!isChatVisible && (
         <div onClick={() => setIsChatVisible(true)}>Open Chat</div>
-        )}
+      )}
     </div>
+    </>
   );
 };
 
