501da712b92243af3951419ecea6d99dbf93c92a
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-01-31 23:54:06 -0500

    add middleware to add and get tasks from database

---
+++ src/components/store/middleware/getAllTasksMiddleware.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import fetchApi from '../../../utility/fetchApi';
+const getAllTasks = (store) => (next) => async (action) => {
+  if (action.payload?.action === 'GET_TASKS') {
+    let url = `${process.env.REACT_APP_SERVER}/tasks`;
+    let body = null;
+    let method = 'get';
+    let config = null;
+    let response = await fetchApi(url, body, method, config);
+    action.payload.response = response;
+  }
+  next(action);
+};
+
+export default getAllTasks;
