9f09e5aa1bb9f565ce1419bfd244b018ca5eb9f1
Stevenrej <steveo732@gmail.com>
2023-02-01 13:25:25 -0600

    updated to properly obtain user's connections data

--- src/components/store/middleware/getConnectionNames.js
+++ src/components/store/middleware/getConnectionNames.js
@@ -1,18 +1,35 @@
-/** @format */
-
 import fetchApi from '../../../utility/fetchApi';
 const convertIdToName = (store) => (next) => async (action) => {
   if (action.payload?.action === 'CONNECTION_NAMES') {
-    const { usersConnections } = store.getState((state) => state).login;
+    const { chatConnection } = store.getState((state) => state).chat;
+    const { user } = store.getState((state) => state).login;
+    let filterConnections = chatConnection.filter((item) => user._id === item.mentor || user._id === item.protege);
 
-    const connectionNames = usersConnections?.map(async (connection) => {
-      let url = `${process.env.REACT_APP_SERVER}/users/${connection}`;
-      let body = null;
-      let method = 'get';
-      let config = null;
-      let response = await fetchApi(url, body, method, config);
-      return response;
-    });
+    const connectionNames = [];
+
+    async function bigFunction(){
+      const grabConnectionPromises = filterConnections.map(async (connection) => {
+        let role;
+        if(user.role === 'mentor'){
+          role = 'protege'
+        }
+        else if(user.role === 'protege'){
+          role = 'mentor'
+        }
+        return grabConnection(role);
+        async function grabConnection(role){
+          let url = `${process.env.REACT_APP_SERVER}/users/${connection[role]}`;
+          let body = null;
+          let method = 'get';
+          let config = null;
+          let response = await fetchApi(url, body, method, config);
+          connectionNames.push(response);
+          return response;
+        }
+      });
+      await Promise.all(grabConnectionPromises);
+    }
+    await bigFunction();
     action.payload.users = connectionNames;
   }
   next(action);
