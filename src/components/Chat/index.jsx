3c877c6194959bec385af4530b79cbdc3d88a4b7
Stevenrej <steveo732@gmail.com>
2023-02-02 12:30:51 -0600

    updated styling and User online functionality

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { joinRoomThunk, sendMessageThunk, getChats } from '../store/chatSlice';
-import { Accordion} from '@mantine/core';
+import { Accordion, Card, Indicator, Stack } from '@mantine/core';
 import io from 'socket.io-client';
 import { convertToNames } from '../store/loginSlice';
 const socket = io.connect(process.env.REACT_APP_SERVER);
@@ -15,9 +15,7 @@ const Chat = () => {
 
   const [showMessage, setShowMessage] = useState(false);
   const [textHeader, setTextHeader] = useState(false);
-  // const [online, setOnline] = useState(false);
-
-
+  const [online, setOnline] = useState([]);
 
   const filterConnection = chatConnection.filter((item) => user._id === item.mentor || user._id === item.protege);
   const nameConnect = userConnectionsUsers.filter(item =>
@@ -30,55 +28,45 @@ const Chat = () => {
     _id: filterConnection.find(filter => item.id === filter.mentor || item.id === filter.protege)._id
   }));
 
-
-
-  function getNames(nameConnect) {
-    return nameConnect.map(item => item.name);
-  }
-
-  function getConnectionId(nameConnect) {
-    return nameConnect.map(item => item.id);
-  }
-
-
-
-  const namesArray = getNames(nameConnect);
-  
   let name = user.name
-  
+
   useEffect(() => {
     dispatch(getChats({ action: 'getChats' }))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  
+
   useEffect(() => {
     dispatch(convertToNames({ action: 'CONNECTION_NAMES' }));
-    
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [chatConnection])
-  
+
   useEffect(() => {
     socket.emit('JOIN', { id: crypto.randomUUID(), userId: user.id })
     socket.on('RECEIVE_MESSAGE', ({ text, id }) => {
-      
+
       dispatch(sendMessageThunk({ text: text, id: id }));
     });
     socket.on('USER_CONNECTED', (data) => {
     });
-    socket.on('proofOfLife', (data) => {
-    });
     socket.on('ROOMS', (data) => {
-      // setOnline(data);
+      setOnline(data);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socket]);
-  
-  // HERE IS USER ONLINE DATA !!!!!!!!!!
-  
-  const idArray = getConnectionId(nameConnect);
-  // console.log("online state", online);
-  console.log(idArray);
-  
+
+  function matchIds(online, nameConnect) {
+    return nameConnect.map(nc => {
+      const match = online.find(o => o.userId === nc.id);
+      return {
+        ...nc,
+        color: match ? 'green' : 'red'
+      };
+    });
+  }
+
+  const matchedId = matchIds(online, nameConnect)
+
   function handleSubmitMessage(e) {
     e.preventDefault();
     if (roomName) {
@@ -92,28 +80,52 @@ const Chat = () => {
     e.target.text.value = ''
   }
 
-
   function handleJoinRoom(e) {
     let name = e.target.innerText
-    setTextHeader(name)
     let text = nameConnect.find(item => item.name === name)._id;
-    socket.emit("LEAVE_ROOM", roomName);
-    dispatch(sendMessageThunk({ id: null }));
-    dispatch(joinRoomThunk({ socket, text }));
+
     setShowMessage(true);
+    if (roomName !== text) {
+      setTextHeader(name)
+      socket.emit("LEAVE_ROOM", roomName);
+      dispatch(sendMessageThunk({ id: null }));
+      dispatch(joinRoomThunk({ socket, text }));
+    } else {
+      return;
+    }
   }
 
   return (
     <>
-      <Accordion variant="contained" className='chat__select'>
+      <Accordion  styles={{
+        item: {
+          backgroundColor: '#fff',
+          border: '1px solid #ededed',
+          '&[data-active]': {
+            backgroundColor: '#ccc',
+          },
+        },
+        chevron: {
+          '&[data-rotate]': {
+            transform: 'rotate(-90deg)',
+          },
+        },
+      }} className='select'>
         <Accordion.Item value="Messages">
-
           <Accordion.Control>Messages</Accordion.Control>
           <Accordion.Panel>
             <p>Your Connections:</p>
-            {namesArray.map(item => (
-              <p onClick={e => handleJoinRoom(e)}
-                key={item}>{item}</p>
+            {matchedId.map(item => (
+              <>
+                <Card withBorder mb={0}>
+                  <Stack withBorder>
+                    <Indicator offset={15} position="middle-end" color={item.color}>
+                      <p onClick={e => handleJoinRoom(e)}
+                        key={item._id}>{item.name}</p>
+                    </Indicator>
+                  </Stack>
+                </Card>
+              </>
             ))}
           </Accordion.Panel>
         </Accordion.Item>
@@ -124,7 +136,6 @@ const Chat = () => {
             <div className="chat__headerBackground"></div>
             <p className="close_button " onClick={() => setShowMessage(false)}>X</p>
             <p className="chat__header">{textHeader} </p>
-
             <div className="chat__message">
               <form onSubmit={handleSubmitMessage}>
                 <textarea name="text" />
