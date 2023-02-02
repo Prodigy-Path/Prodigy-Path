5d9ea683003f4bc472e2987132a608c1f4c9bc64
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-02 00:07:14 -0500

    tests connectionMiddleware

---
+++ src/components/store/middleware/connection.test.js
@@ -0,0 +1,36 @@
+/** @format */
+
+import connectionMiddleware from './connectionMiddleware';
+
+jest.mock('../../../utility/fetchApi', () => jest.fn(() => Promise.resolve()));
+describe('connectionMiddleware', () => {
+  it('should call next with action if action is not a connection request', () => {
+    const store = {};
+    const next = jest.fn();
+    const action = {
+      payload: {
+        action: 'not-connection',
+      },
+    };
+    connectionMiddleware(store)(next)(action);
+    expect(next).toHaveBeenCalledWith(action);
+  });
+
+  it('should add newConnection to connection_requests and call next with updated action', async () => {
+    const store = {};
+    const next = jest.fn();
+    const action = {
+      payload: {
+        action: 'connection',
+        connection_requests: ['request1', 'request2'],
+        user: {
+          _id: 'user_id',
+        },
+        newConnection: 'new_request',
+        token: 'token',
+      },
+    };
+    await connectionMiddleware(store)(next)(action);
+    expect(next).toHaveBeenCalled();
+  });
+});
